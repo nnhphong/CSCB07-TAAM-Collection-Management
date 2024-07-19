be05677990d457daba16743b5bdc2dc1bfb49954
Paul <nnhoangphong@gmail.com>
2024-07-19 06:40:03 -0400

    feat: add demo search screen

---
+++ app/src/main/java/com/example/b07demosummer2024/SearchFragment.java
@@ -0,0 +1,27 @@
+package com.example.b07demosummer2024;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SearchFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        Button btnTop = view.findViewById(R.id.btnTop);
+
+        btnTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getParentFragmentManager().popBackStack();
+            }
+        });
+
+        return view;
+    }
+}
