bc31a99b8badc785562ef2be6ae604701bb5c01e
Paul <nnhoangphong@gmail.com>
2024-07-22 13:53:38 -0400

    fix: fix merge conflict searchfragment.java

--- app/src/main/java/com/example/b07demosummer2024/SearchFragment.java
+++ app/src/main/java/com/example/b07demosummer2024/SearchFragment.java
@@ -1,13 +1,18 @@
 package com.example.b07demosummer2024;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,19 +22,33 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import java.util.List;
 import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class SearchFragment extends Fragment {
     private FirebaseDatabase db;
-    private DatabaseReference itemsRef;
-    private Button buttonSearch;
+    private DBOperation op;
+    public void addDropDownValue(View view, int dropDownID, int arrValuesID) {
+        Spinner spinner = view.findViewById(dropDownID);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                arrValuesID, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
         Button btnTop = view.findViewById(R.id.btnTop);
-        buttonSearch = view.findViewById(R.id.btnSearch);
+        ImageButton btnSearch = view.findViewById(R.id.btnSearch);
+
+        addDropDownValue(view, R.id.dropDownCategory, R.array.arr_category);
+        addDropDownValue(view, R.id.dropDownPeriod, R.array.arr_period);
 
         db = FirebaseDatabase.getInstance("https://cscb07-taam-management-default-rtdb.firebaseio.com/");
+        DatabaseReference ref = db.getReference("testDemo");
+        op = new DBOperation(ref);
 
         btnTop.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,16 +57,44 @@ public class SearchFragment extends Fragment {
             }
         });
 
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText txtLotNumber = view.findViewById(R.id.txtLotNumber);
+                EditText txtName = view.findViewById(R.id.txtName);
+                TextView txtStatus = view.findViewById(R.id.txtStatus);
+                Spinner dropDownCategory = view.findViewById(R.id.dropDownCategory);
+                Spinner dropDownPeriod = view.findViewById(R.id.dropDownPeriod);
+
+                String strLotNumber = txtLotNumber.getText().toString();
+                String name = txtName.getText().toString();
+                Integer lotNum = null;
+                if (!strLotNumber.isEmpty()) {
+                    try {
+                        lotNum = Integer.parseInt(strLotNumber);
+                    } catch (NumberFormatException e) {
+                        txtStatus.setText("Error: Lot number is not a number");
+                        txtStatus.setTextColor(Color.parseColor("#f54242"));
+                        return;
+                    }
+                }
+                String selectedCategory = dropDownCategory.getSelectedItem().toString();
+                String selectedPeriod = dropDownPeriod.getSelectedItem().toString();
+
+                Item item = new Item(lotNum, name, selectedCategory, selectedPeriod, "");
+//                System.out.println(item.getLotNumber() + " " + item.getName() + " " + item.getCategory() + " " + item.getPeriod());
+                List<Item> result = op.searchAndDisplay(item);
+                displayInfo(result);
+            }
+        });
+
         ValueEventListener postListener = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 Item it = snapshot.getValue(Item.class);
-                if (it != null)
-                {
+                if (it != null) {
                     System.out.println(it.toString());
-                }
-                else
-                {
+                } else {
                     System.out.println("load post:onCancelled");
                 }
             }
@@ -58,21 +105,27 @@ public class SearchFragment extends Fragment {
         };
         return view;
     }
-    private void searchItem() {
-        itemsRef = db.getReference();
+
+    private void displayInfo(List<Item> l) {
+        for (Item item : l) {
+            System.out.println(item.getLotNumber() + " " + item.getName() + " " + item.getCategory() + " " + item.getPeriod());
+        }
     }
-    public View displaySearchRes(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState, List<Item> l) {
-	    View view = inflater.inflate(R.layout.fragment_recycler_view, container, false);
-        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
-        setAdapter();
-	    return view;
-    }
-	
-    public void setAdapter() {
-        ItemAdapter itemAdapter = new ItemAdapter(itemList);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(itemAdapter);
-    }
-    
+
+//    private void searchItem() {
+//        itemsRef = db.getReference();
+//    }
+//    public View displaySearchRes(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState, List<Item> l) {
+//        View view = inflater.inflate(R.layout.fragment_recycler_view, container, false);
+//        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+//        setAdapter();
+//        return view;
+//    }
+//
+//    public void setAdapter() {
+//        ItemAdapter itemAdapter = new ItemAdapter(itemList);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+//        recyclerView.setItemAnimator(new DefaultItemAnimator());
+//        recyclerView.setAdapter(itemAdapter);
+//    }
 }
