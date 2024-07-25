e49a7580aa00754b489da084ef26da5a733f31e7
taeminlee0601 <taeminlee0601@gmail.com>
2024-07-24 22:47:06 -0400

    feat: added responsive spinners (in progress)

--- app/src/main/java/com/example/b07demosummer2024/AddItemFragment.java
+++ app/src/main/java/com/example/b07demosummer2024/AddItemFragment.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.content.ContentResolver;
 import android.database.Cursor;
 import android.provider.OpenableColumns;
 
@@ -27,15 +26,12 @@ import androidx.annotation.Nullable;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
