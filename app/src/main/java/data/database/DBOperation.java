3b09b76206f999517db4355bee1ba38b3ec19a34
taeminlee0601 <taeminlee0601@gmail.com>
2024-07-26 02:19:37 -0400

    feat: implemented alert dialog + removing item from database

--- app/src/main/java/data/database/DBOperation.java
+++ app/src/main/java/data/database/DBOperation.java
@@ -2,8 +2,12 @@ package data.database;
 
 import android.net.Uri;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.TaskCompletionSource;
 
