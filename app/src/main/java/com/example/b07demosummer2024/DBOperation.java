429e7ae3eb609f72908eebc13cbd5903abbc5c5e
hailongng <hailong.nguyen@mail.utoronto.ca>
2024-07-27 18:33:46 -0400

    feat: Login page
    Still needs to connect to the database

--- app/src/main/java/com/example/b07demosummer2024/DBOperation.java
+++ app/src/main/java/com/example/b07demosummer2024/DBOperation.java
@@ -1,168 +1,43 @@
 package com.example.b07demosummer2024;
 
-import android.net.Uri;
 import android.provider.ContactsContract;
-
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.tasks.Continuation;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.TaskCompletionSource;
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
-
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
-import java.util.Collections;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 
 public class DBOperation {
     private DatabaseReference ref;
-    private StorageReference mediaRef;
-
     public DBOperation(DatabaseReference ref) {
         this.ref = ref;
     }
 
-    public DBOperation(DatabaseReference ref, StorageReference mediaRef) {
-        this.ref = ref;
-        this.mediaRef = mediaRef;
-    }
-
-    public Task<List<String>> getCategories() {
-        List<String> categories = new ArrayList<>();
-        categories.add("");
-        TaskCompletionSource<List<String>> tcs = new TaskCompletionSource<>();
-
-        ref.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot data : snapshot.getChildren()) {
-                    Item item = data.getValue(Item.class);
-                    if (item == null) {
-                        System.out.println("Item is null!");
-                        continue;
-                    }
-
-                    if (!categories.contains(item.getCategory())) {
-                        categories.add(item.getCategory());
-                    }
-                }
-
-                tcs.setResult(categories);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                System.out.println("WTF");
-            }
-        });
-
-        return tcs.getTask();
-    }
-
-    public Task<List<String>> getPeriods() {
-        List<String> periods = new ArrayList<>();
-        periods.add("");
-        TaskCompletionSource<List<String>> tcs = new TaskCompletionSource<>();
-
-        ref.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot data : snapshot.getChildren()) {
-                    Item item = data.getValue(Item.class);
-                    if (item == null) {
-                        System.out.println("Item is null!");
-                        continue;
-                    }
-
-                    if (!periods.contains(item.getPeriod())) {
-                        periods.add(item.getPeriod());
-                    }
-                }
-
-                tcs.setResult(periods);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                System.out.println("WTF");
-            }
-        });
-
-        return tcs.getTask();
-    }
-
-    public UploadTask addImage(Uri selectedMedia, AddItemFragment fragment, int lotNumber) {
-        String id = "id" + lotNumber;
-        StorageReference media = mediaRef.child(id);
-        UploadTask uploadTask = media.putFile(selectedMedia);
-
-        return uploadTask;
-    }
-  
     public Task<Void> addItem(Item item, AddItemFragment fragment) {
         String id = "id" + item.getLotNumber();
 
-        // Only store these fields in the database
-        Map<String, Object> itemMap = new HashMap<>();
-        itemMap.put("lotNumber", item.getLotNumber());
-        itemMap.put("name", item.getName());
-        itemMap.put("category", item.getCategory());
-        itemMap.put("period", item.getPeriod());
-        itemMap.put("description", item.getDescription());
-        itemMap.put("mediaLink", item.getMediaLink());
-
-        return ref.child(id).setValue(itemMap).addOnCompleteListener(task -> {
-        });
-    }
-
-    private String buildRegex(String find) {
-        String [] words = find.toLowerCase().split(" ");
-        StringBuilder regex = new StringBuilder("\\b(");
-        for (String word : words) {
-            regex.append("\\w*").append(word).append("\\w*");
-            if (word != words[words.length - 1]) {
-                regex.append("|");
+        return ref.child(id).setValue(item).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                fragment.displayToast("Item added successfully");
+            } else {
+                fragment.displayToast("Failed to add item");
             }
-        }
-        regex.append(")\\b");
-        return regex.toString();
-    }
-
-    private Boolean matchByRegex(String target, String find) {
-        String regex = buildRegex(find);
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(target.toLowerCase());
-        while (matcher.find()) {
-            // if there is at least one instance, return True
-            return true;
-        }
-        return false;
+        });
     }
 
     public Task<List<Item>> searchItem(Item criteria) {
         List<Item> filteredItem = new ArrayList<>();
         TaskCompletionSource<List<Item>> tcs = new TaskCompletionSource<>();
-
         ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -172,14 +47,12 @@ public class DBOperation {
                         System.out.println("Item is null!");
                         continue;
                     }
-
                     if (criteria.getLotNumber() != null && item.getLotNumber() != null &&
                             !Objects.equals(item.getLotNumber(), criteria.getLotNumber())) {
                         continue;
                     }
-
                     if (!criteria.getName().isEmpty() && !item.getName().isEmpty() &&
-                            !matchByRegex(item.getName(), criteria.getName())) {
+                            !Objects.equals(criteria.getName(), item.getName())) {
                         continue;
                     }
                     if (!criteria.getCategory().isEmpty() &&
@@ -192,53 +65,55 @@ public class DBOperation {
                     }
                     filteredItem.add(item);
                 }
-
                 tcs.setResult(filteredItem);
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 System.out.println("WTF");
             }
         });
-
         return tcs.getTask();
     }
-
-    public Task<Void> removeImage(Item item) {
-        String mediaLink = item.getMediaLink();
-
-        return mediaRef.child(mediaLink).delete().addOnCompleteListener(task -> {
-        });
-    }
-
-    public Task<Void> removeItem(Item item) {
+    /*
+    public Task<Void> removeItem(Item item, RemoveItemFragment fragment) {
         String id = "id" + item.getLotNumber();
 
         return ref.child(id).removeValue().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                fragment.displayToast("Item deleted successfully");
+            } else {
+                fragment.displayToast("Failed to delete item");
+            }
         });
     }
+    */
 
-    public void loadItems(List<Item> items, ItemAdapter itemAdapter) {
-        ref.addValueEventListener(new ValueEventListener() {
+    public Task<List<User>> login(String username, String password) {
+        TaskCompletionSource<List<User>> tcs = new TaskCompletionSource<>();
+        List<User> user_list = new ArrayList<>();
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                items.clear();
-                for (DataSnapshot data : snapshot.getChildren()) {
-                    Item item = data.getValue(Item.class);
-                    if (item != null) {
-                        items.add(item);
-                        System.out.println(item.getMediaLink());
+                for (DataSnapshot users : snapshot.getChildren()) {
+                    User user = users.getValue(User.class);
+                    if (user == null) {
+                        continue;
                     }
+                    if (!user.getUsername().isEmpty() && !username.isEmpty() &&
+                            !Objects.equals(user.getUsername(), username)) {
+                        continue;
+                    }
+                    if (!user.getPassword().isEmpty() && !password.isEmpty() &&
+                            !Objects.equals(user.getPassword(), password)) {
+                        continue;
+                    }
+                    user_list.add(user);
                 }
-                Collections.sort(items);
-                itemAdapter.notifyDataSetChanged();
+                tcs.setResult(user_list);
             }
-
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                System.out.println("WTF");
-            }
+            public void onCancelled(@NonNull DatabaseError error) {}
         });
+        return tcs.getTask();
     }
 }
