d49cce2d8744bef17b28dac709ced689e136d885
Paul <nnhoangphong@gmail.com>
2024-07-27 09:07:52 -0400

    feat: add writing to pdf code

---
+++ app/src/main/java/com/example/b07demosummer2024/PDFGenerator.java
@@ -0,0 +1,158 @@
+package com.example.b07demosummer2024;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.pdf.PdfDocument;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskCompletionSource;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.BiConsumer;
+
+public class PDFGenerator {
+    Fragment curFrag;
+    private int pageHeight = 1120;
+    private int pageWidth = 792;
+    private int imgHeight = 200;
+    private int imgWidth = 140;
+    private static final int PERMISSION_REQUEST_CODE = 200;
+    Bitmap bmp, scaledbmp;
+    StorageReference ref;
+    private final ExecutorService executor = Executors.newFixedThreadPool(4);
+
+    public PDFGenerator(Fragment curFrag) {
+        this.curFrag = curFrag;
+    }
+
+    public void createReportPDF(List<Item> list, boolean descImgOnly) {
+        ref = FirebaseStorage.getInstance("gs://cscb07-taam-management.appspot.com").getReference();
+        PdfDocument pdfDocument = new PdfDocument();
+        Paint paint = new Paint();
+
+        drawAllItems(pdfDocument, paint, list).whenComplete(new BiConsumer<Void, Throwable>() {
+            @Override
+            public void accept(Void unused, Throwable throwable) {
+                File file = new File(Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_DOWNLOADS), getPDFFileName());
+
+                try {
+                    pdfDocument.writeTo(new FileOutputStream(file));
+                    Toast.makeText(curFrag.getContext(), "PDF file generated successfully.",
+                            Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    Toast.makeText(curFrag.getContext(), "Failed to generate PDF file.",
+                            Toast.LENGTH_SHORT).show();
+                }
+                System.out.println("Im here");
+                pdfDocument.close();
+                executor.shutdown();
+            }
+        });
+    }
+
+    private CompletableFuture<Void> drawAllItems(PdfDocument pdfDocument, Paint paint, List<Item> list) {
+        CompletableFuture<Void> future = CompletableFuture.completedFuture(null);
+        for (Item item : list) {
+            future = future.thenCompose(avoid -> drawItem(pdfDocument, paint, item));
+        }
+        return future;
+    }
+
+    private CompletableFuture<Void> drawItem(PdfDocument pdfDocument, Paint paint, Item item) {
+        return CompletableFuture.runAsync(() -> {
+            ref.child(item.getMediaLink()).getBytes(Long.MAX_VALUE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                @Override
+                public void onSuccess(byte[] bytes) {
+                    PdfDocument.PageInfo mypageInfo = new PdfDocument.PageInfo.Builder(pageWidth,
+                            pageHeight, 1).create();
+                    PdfDocument.Page myPage = pdfDocument.startPage(mypageInfo);
+                    Canvas canvas = myPage.getCanvas();
+                    InputStream inputStream = new ByteArrayInputStream(bytes);
+                    bmp = BitmapFactory.decodeStream(inputStream);
+                    scaledbmp = Bitmap.createScaledBitmap(bmp, imgWidth, imgHeight, false);
+
+                    if (scaledbmp != null) {
+                        canvas.drawBitmap(scaledbmp, 20, pageHeight / 3, paint);
+                    } else {
+                        Log.e("generatePDF", "Bitmap is null. Skipping bitmap drawing.");
+                    }
+
+                    drawText(canvas, item.getName(), pageWidth / 2, 25*2, 25);
+                    drawText(canvas, item.getPeriod(), 50, 20, 15);
+                    drawText(canvas, item.getCategory(), 30, 20, 15);
+                    drawText(canvas, item.getDescription(), pageWidth / 2 + 2, 80, 15);
+
+                    pdfDocument.finishPage(myPage);
+                }
+            });
+        }, executor);
+    }
+
+    private void drawText(Canvas canvas, String text, int x, int y, int fontSize) {
+        Paint paint = new Paint();
+        paint.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+        paint.setColor(ContextCompat.getColor(curFrag.getContext(), R.color.black));
+        paint.setTextSize(fontSize);
+//        paint.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText(text, x, y, paint);
+    }
+
+    private String getPDFFileName() {
+        Date now = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        String formattedDateTime = sdf.format(now);
+        return formattedDateTime + "_report.pdf";
+    }
+
+    public boolean checkPermission() {
+        // checking of permissions.
+        int permission1 = ContextCompat.checkSelfPermission(curFrag.getContext(), WRITE_EXTERNAL_STORAGE);
+        int permission2 = ContextCompat.checkSelfPermission(curFrag.getContext(), READ_EXTERNAL_STORAGE);
+        return permission1 == PackageManager.PERMISSION_GRANTED && permission2
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public void requestPermission() {
+        ActivityCompat.requestPermissions(curFrag.getActivity(), new String[]{WRITE_EXTERNAL_STORAGE,
+                READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+    }
+}
--- app/src/main/java/com/example/b07demosummer2024/ReportFragment.java
+++ app/src/main/java/com/example/b07demosummer2024/ReportFragment.java
@@ -1,5 +1,6 @@
 package com.example.b07demosummer2024;
 
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
@@ -7,6 +8,8 @@ import android.graphics.pdf.PdfDocument;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,26 +24,39 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import android.graphics.Canvas;
+import android.widget.Toast;
+
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class ReportFragment extends Fragment {
-    private int pageHeight = 1120;
-    private int pageWidth = 792;
-    private static final int PERMISSION_REQUEST_CODE = 200;
-
     FirebaseDatabase db;
     DBOperation op;
+    Bitmap bmp, scaledbmp;
+    PDFGenerator pdfWriter;
 
     @Nullable
     @Override
@@ -48,10 +64,11 @@ public class ReportFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_report, container, false);
         ImageButton btnTop = view.findViewById(R.id.btnTop);
         Button btnGenReport = view.findViewById(R.id.btnGenReport);
-        Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.taam_logo);
 
         db = FirebaseDatabase.getInstance();
+        StorageReference ref = FirebaseStorage.getInstance("gs://cscb07-taam-management.appspot.com").getReference();
         op = new DBOperation(db.getReference("/data"));
+        pdfWriter = new PDFGenerator(this);
 
         btnTop.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,72 +77,62 @@ public class ReportFragment extends Fragment {
             }
         });
 
-        btnGenReport.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText txtLotNumber = view.findViewById(R.id.txtLotNumber);
-                EditText txtName = view.findViewById(R.id.txtName);
-                TextView txtStatus = view.findViewById(R.id.txtStatus);
-                Spinner dropDownCategory = view.findViewById(R.id.dropDownCategory);
-                Spinner dropDownPeriod = view.findViewById(R.id.dropDownPeriod);
-                CheckBox ckbDescImgOnly = view.findViewById(R.id.ckbDescPicOnly);
-                final boolean[] descImgOnly = {true};
+        if (pdfWriter.checkPermission()) {
+            Toast.makeText(getContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
+        } else {
+            pdfWriter.requestPermission();
+        }
 
-                String strLotNumber = txtLotNumber.getText().toString();
-                String name = txtName.getText().toString();
-                Integer lotNum = null;
-                if (!strLotNumber.isEmpty()) {
-                    try {
-                        lotNum = Integer.parseInt(strLotNumber);
-                    } catch (NumberFormatException e) {
-                        txtStatus.setText("Error: Lot number is not a number");
-                        txtStatus.setTextColor(Color.parseColor("#f54242"));
-                        return;
-                    }
-                }
+//        btnGenReport.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                EditText txtLotNumber = view.findViewById(R.id.txtLotNumber);
+//                EditText txtName = view.findViewById(R.id.txtName);
+//                TextView txtStatus = view.findViewById(R.id.txtStatus);
+//                Spinner dropDownCategory = view.findViewById(R.id.dropDownCategory);
+//                Spinner dropDownPeriod = view.findViewById(R.id.dropDownPeriod);
+//                CheckBox ckbDescImgOnly = view.findViewById(R.id.ckbDescPicOnly);
+//                final boolean[] descImgOnly = {true};
+//
+//                String strLotNumber = txtLotNumber.getText().toString();
+//                String name = txtName.getText().toString();
+//                Integer lotNum = null;
+//                if (!strLotNumber.isEmpty()) {
+//                    try {
+//                        lotNum = Integer.parseInt(strLotNumber);
+//                    } catch (NumberFormatException e) {
+//                        txtStatus.setText("Error: Lot number is not a number");
+//                        txtStatus.setTextColor(Color.parseColor("#f54242"));
+//                        return;
+//                    }
+//                }
+//
+//                String selectedCategory = dropDownCategory.getSelectedItem().toString();
+//                String selectedPeriod = dropDownPeriod.getSelectedItem().toString();
+//
+//                Item item = new Item(lotNum, name, selectedCategory, selectedPeriod, "");
+//                ckbDescImgOnly.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//                    @Override
+//                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                        descImgOnly[0] = isChecked;
+//                    }
+//                });
+//                op.searchItem(item).addOnCompleteListener(new OnCompleteListener<List<Item>>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<List<Item>> task) {
+//                        List<Item> result = task.getResult();
+//                        pdfWriter.createReportPDF(result, descImgOnly[0]);
+//                    }
+//                });
+//            }
+//        });
 
-                String selectedCategory = dropDownCategory.getSelectedItem().toString();
-                String selectedPeriod = dropDownPeriod.getSelectedItem().toString();
-
-                Item item = new Item(lotNum, name, selectedCategory, selectedPeriod, "");
-                ckbDescImgOnly.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        descImgOnly[0] = isChecked;
-                    }
-                });
-                op.searchItem(item).addOnCompleteListener(new OnCompleteListener<List<Item>>() {
-                    @Override
-                    public void onComplete(@NonNull Task<List<Item>> task) {
-                        List<Item> result = task.getResult();
-                        createReportPDF(result, descImgOnly[0]);
-                    }
-                });
-            }
-        });
+        List<Item> result = new ArrayList<>();
+        Item item = new Item(69, "Tea Man art", "Paintings", "Ming", "This is Tea Man art masterpiece");
+        item.setMediaLink("media/id5");
+        result.add(item);
+        pdfWriter.createReportPDF(result, false);
 
         return view;
-    }
-
-    private void createReportPDF(List<Item> list, boolean descImgOnly) {
-        PdfDocument pdf = new PdfDocument();
-        Paint paint = new Paint();
-        Paint title = new Paint();
-
-        PdfDocument.PageInfo myPageInfo = new PdfDocument.PageInfo.Builder(this.pageWidth, this.pageHeight, 1).create();
-        PdfDocument.Page myPage = pdf.startPage(myPageInfo);
-        Canvas canvas = myPage.getCanvas();
-
-
-        title.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
-        title.setTextSize(15);
-//        title.setColor(ContextCompat.getColor(this, R.color.black));
-        canvas.drawText("A portal for IT professionals.", 209, 100, title);
-        canvas.drawText("Geeks for Geeks", 209, 80, title);
-    }
-
-    private void requestPermission() {
-        // requesting permissions if not provided.
-//        ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
     }
 }
